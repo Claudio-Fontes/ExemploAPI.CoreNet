c72af49a0e523d9d02083b3846d0014e3a6db1fe
Claudio-Fontes <claudio.melo.android@gmail.com>
2017-11-09 12:07:10 -0200

    testes cmmit

--- ExemploAPI.CoreNet/Startup.cs
+++ ExemploAPI.CoreNet/Startup.cs
@@ -25,8 +25,6 @@ namespace ExemploAPI.CoreNet
         {
             services.AddMvc();
         }
-
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
