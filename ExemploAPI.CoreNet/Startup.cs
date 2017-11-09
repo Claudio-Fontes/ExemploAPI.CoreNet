25afc48f79cdb4187ecfc188551c893b34d043a9
Claudio-Fontes <claudio.melo.android@gmail.com>
2017-11-09 11:32:58 -0200

    sdsds

--- ExemploAPI.CoreNet/Startup.cs
+++ ExemploAPI.CoreNet/Startup.cs
@@ -28,6 +28,8 @@ namespace ExemploAPI.CoreNet
 
 
 
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
